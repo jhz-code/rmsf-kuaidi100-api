b01524398752d72aabe4594fb64b1e9a9139af57
zjh <zhuok520@qq.com>
2021-06-26 14:28:17 +0800

    打印接口封装

--- src/core/TopKuaidi100.php
+++ src/core/TopKuaidi100.php
@@ -10,10 +10,12 @@ namespace RmTop\RmKuaidi100\core;
 
 
 use GuzzleHttp\Exception\GuzzleException;
+use RmTop\RmKuaidi100\lib\TopEPrinter;
 use RmTop\RmKuaidi100\lib\TopKuaidi100Manage;
 use RmTop\RmKuaidi100\lib\TopSign;
 use RmTop\RmKuaidi100\lib\TopParams;
 use RmTop\RmKuaidi100\lib\TopPrinter;
+use RmTop\RmKuaidi100\lib\TopThirdPrinter;
 use RmTop\RmKuaidi100\model\TopKuaidi100ConfigModel;
 use think\db\exception\DataNotFoundException;
 use think\db\exception\DbException;
@@ -35,6 +37,115 @@ class TopKuaidi100
       $this->printer = TopKuaidi100Manage::getConfig($configId);
     }
 
+
+    /**
+     * 一、电子面单打印接口
+    通过快递公司或网点、菜鸟与淘宝提供的电子面单账号，调用打印设备打印输出。
+     * @param $params
+     * @return array
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws GuzzleException
+     * @throws ModelNotFoundException
+     */
+    function eOrder($params): array
+    {
+        $TopClient = new TopPrinter();
+        list($msec, $sec) = explode(' ', microtime());
+        $TIME = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    //当前时间戳
+        $TopClient->setParams([
+            'method'=>"eOrder",
+            'key'=>$this->printer['config']['appKey'],
+            't'=>$TIME,
+            'param'=>TopParams::getParams($params),
+            'sign'=>TopSign::create_sign(TopParams::getParams($params),$TIME,$this->printer['config']['appKey'],$this->printer['config']['appSecret']),
+        ]);
+        return  $TopClient->TopPrint();
+    }
+
+
+    /**
+     * 电子面单图片接口
+    通过快递公司或网点、菜鸟与淘宝提供的电子面单账号，提交生成电子面单；
+     * @param $params
+     * @return array
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws GuzzleException
+     * @throws ModelNotFoundException
+     *
+     */
+    function getEPrintImg($params): array
+    {
+        $TopClient = new TopPrinter();
+        list($msec, $sec) = explode(' ', microtime());
+        $TIME = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    //当前时间戳
+        $TopClient->setParams([
+            'method'=>"getPrintImg",
+            'key'=>$this->printer['config']['appKey'],
+            't'=>$TIME,
+            'param'=>TopParams::getParams($params),
+            'sign'=>TopSign::create_sign(TopParams::getParams($params),$TIME,$this->printer['config']['appKey'],$this->printer['config']['appSecret']),
+        ]);
+        return  $TopClient->TopPrint();
+    }
+
+
+    /**
+     * 电子面单HTML接口
+    通过快递公司或网点、菜鸟与淘宝提供的电子面单账号，提交生成电子面单号，返回固定面单模板，调用本地打印机即可打印出电子面单
+     * @param $params
+     * @return array
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws GuzzleException
+     * @throws ModelNotFoundException
+     */
+    function getElecOrder($params): array
+    {
+        $TopClient = new TopEPrinter();
+        list($msec, $sec) = explode(' ', microtime());
+        $TIME = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    //当前时间戳
+        $TopClient->setParams([
+            'method'=>"getElecOrder",
+            'key'=>$this->printer['config']['appKey'],
+            't'=>$TIME,
+            'param'=>TopParams::getParams($params),
+            'sign'=>TopSign::create_sign(TopParams::getParams($params),$TIME,$this->printer['config']['appKey'],$this->printer['config']['appSecret']),
+        ]);
+        return  $TopClient->TopPrint();
+    }
+
+
+    /**
+     * 国际电子面单
+    一、国际电子面单下单
+    通过国际快递公司或网点提供的电子面单账号，提交生成电子面单号，返回固定面单模板，调用本地打印机即可打印出电子面单
+     * @param $params
+     * @return array
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws GuzzleException
+     * @throws ModelNotFoundException
+     */
+    function interShip($params): array
+    {
+        $TopClient = new TopEPrinter();
+        list($msec, $sec) = explode(' ', microtime());
+        $TIME = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    //当前时间戳
+        $TopClient->setParams([
+            'method'=>"intership",
+            'key'=>$this->printer['config']['appKey'],
+            't'=>$TIME,
+            'param'=>TopParams::getParams($params),
+            'sign'=>TopSign::create_sign(TopParams::getParams($params),$TIME,$this->printer['config']['appKey'],$this->printer['config']['appSecret']),
+        ]);
+        return  $TopClient->TopPrint();
+    }
+
+
+
+    //-------------------------------------------------------- 自定义打印接口 -------------------------------------
 
     /**
      * @param $params
@@ -113,6 +224,7 @@ class TopKuaidi100
     }
 
 
+
     /**
      * 对短期内打印过的面单进行复打操作。 该接口支持在提交打印请求2天内的打印任务进行复打10次的操作。
      * @param $taskId
@@ -158,6 +270,60 @@ class TopKuaidi100
             't'=>$TIME,
             'param'=>TopParams::getParams(['siid'=>$siid]),
             'sign'=>TopSign::create_sign(TopParams::getParams(['siid'=>$siid]),$TIME,$this->printer['config']['appKey'],$this->printer['config']['appSecret']),
+        ]);
+        return  $TopClient->TopPrint();
+    }
+
+
+
+    //------------------------------------------ 第三方授权打印 ------------------------------------------
+
+
+    /**
+     * 第三方电商平台账号授权
+    通过第三方授权获取月结账号授权码
+     * @param $params
+     * @return array
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws GuzzleException
+     * @throws ModelNotFoundException
+     */
+    function authThird($params): array
+    {
+        $TopClient = new TopThirdPrinter();
+        list($msec, $sec) = explode(' ', microtime());
+        $TIME = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    //当前时间戳
+        $TopClient->setParams([
+            'key'=>$this->printer['config']['appKey'],
+            't'=>$TIME,
+            'param'=>TopParams::getParams($params),
+            'sign'=>TopSign::create_sign(TopParams::getParams($params),$TIME,$this->printer['config']['appKey'],$this->printer['config']['appSecret']),
+        ]);
+        return  $TopClient->TopPrint();
+    }
+
+
+    /**
+     * 菜鸟淘宝网点&面单余额接口
+    通过菜鸟淘宝账号授权接口提交的第三方授权成功后，通过该接口可以获取到该授权账户对应的绑定网点信息以及账户可用单量。
+     * @param $params
+     * @return array
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws GuzzleException
+     * @throws ModelNotFoundException
+     */
+    function getThirdInfo($params){
+        $TopClient = new TopEPrinter();
+        list($msec, $sec) = explode(' ', microtime());
+        $TIME = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);    //当前时间戳
+        $TopClient->setParams([
+            'method'=>"getThirdInfo",
+            'key'=>$this->printer['config']['appKey'],
+            't'=>$TIME,
+            'param'=>TopParams::getParams($params),
+            'sign'=>TopSign::create_sign(TopParams::getParams($params),$TIME,$this->printer['config']['appKey'],$this->printer['config']['appSecret']),
         ]);
         return  $TopClient->TopPrint();
     }
---
+++ src/lib/TopThirdPrinter.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace RmTop\RmKuaidi100\lib;
+
+
+use GuzzleHttp\Exception\GuzzleException;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\DbException;
+use think\db\exception\ModelNotFoundException;
+
+class TopThirdPrinter
+{
+
+    protected string  $apiUrl = "https://poll.kuaidi100.com/printapi/authThird.do";
+    protected array  $params;
+
+
+    /**
+     * 公用请求
+     * @return array
+     * @throws GuzzleException
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
+    function TopPrint(): array
+    {
+        $TopClient = new TopClient();
+        $TopClient->setApiUrl($this->apiUrl);
+        $TopClient->setParams($this->params);
+        return $TopClient->Client();
+    }
+
+
+
+
+    function setParams(array $params){
+        $this->params =$params;
+    }
+
+}
